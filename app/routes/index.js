7fd1a82290e50a8275bc0446d263c12369e0958b
mjuniper <mjuniper@gmail.com>
2018-10-02 07:04:20 -0600

    willTransition

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  actions: {
+    willTransition () {
+      this.controllerFor('index').set('sidebarIsOpen', false);
+    }
+  }
+
 });
